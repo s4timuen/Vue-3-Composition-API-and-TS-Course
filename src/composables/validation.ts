7734f4aa164c2b14827a97a01f47e7f7344f6167
s4timuen <muenzinger.tim@googlemail.com>
2024-05-15 10:42:42 +0200

    added length validation and more type safety

--- src/composables/validation.ts
+++ src/composables/validation.ts
@@ -3,15 +3,29 @@ interface Status {
     message?: string
 }
 
-// rules
+/**
+ * Rules
+ * @param {string} value
+ * @return {Status}
+ */
 type Rule = (value: string) => Status;
 
-function required(value: string): Status {
+const required: Rule = (value: string): Status => {
     const result: boolean = Boolean(value);
     return {
         valid: result,
         message: result ? undefined : 'This field is required'
     };
+}
+
+function length({ min, max }: { min: number, max: number }): Rule {
+    return function (value: string): Status {
+        const result = Boolean(value.length >= min && value.length <= max);
+        return {
+            valid: result,
+            message: result ? undefined : `This fields length must be between ${min} and ${max}`
+        }
+    }
 }
 
 // validation
@@ -25,4 +39,4 @@ function validate(value: string, rules: Rule[]): Status {
     return { valid: true };
 }
 
-export { required, validate };
+export { length, required, validate };
